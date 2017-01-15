ae6ab721aaf35caef909c9d0747b422e104a5e60
Vaibhav Srivastava <vabnix@hotmail.com>
2017-01-15 13:56:37 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
     public static void main(String args[]) {
 
-        Olives o1 = new Olives("Vaibhav");
-        Olives o2 = new Olives("Pallavi");
+        Olives o1 = new Olives("India");
+        Olives o2 = new Olives("London");
         Olives o3 = new Olives("Dallas");
 
         Olive[] = {o1, o2, o3};
